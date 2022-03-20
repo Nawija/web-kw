a9673b6bcd63737c38df164168956a78a556dd7f
Kondziu <kondziu@MacBook-Air-Kondziu.local>
2022-03-20 10:29:19 +0100

    s

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+audio = document.querySelector("$audio");
+
+document.addEventListener("DOMContentLoaded", function () {
+    audioElement.play(audio);
+});
+
+let hasPlayed = true;
+function handleFirstPlay(event) {
+    document.addEventListener("DOMContentLoaded"), function(){
+        audioElement.play(audio);
+
+    }
+}
