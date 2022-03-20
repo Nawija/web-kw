e8a185c331b2d3190c99d9cc3829f1260242c930
Kondziu <kondziu@MacBook-Air-Kondziu.local>
2022-03-20 10:42:33 +0100

    s

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,3 @@
-audio = document.querySelector("$audio");
-
-document.addEventListener("DOMContentLoaded", function () {
-    audioElement.play(audio);
-});
-
-let hasPlayed = true;
-function handleFirstPlay(event) {
-    document.addEventListener("DOMContentLoaded"), function(){
-        audioElement.play(audio);
-
-    }
-}
+setTimeout(function(){
+    document.getElementById("my-audio").play();
+},8000)
